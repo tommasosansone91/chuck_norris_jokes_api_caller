6913a3955a04fb47b5a52fbdb8053c3df40a2974
tommasosansone91 <tommaso.sansone91@virgilio.it>
2024-10-27 17:17:57 +0100

    add test request api

--- chuck_norris_jokes_caller/app.py
+++ chuck_norris_jokes_caller/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask import request
 
 from markupsafe import escape
  
@@ -8,10 +9,18 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-# a simple page that says hello
+
 @app.route('/hello')
 def hello():
     return '<h1>Welcome on Chuck Norris Jokes app!</h1>'
     
+
+@app.route('/helloname/')
+def helloname():
+    name = request.args.get('name', 'You')
+    # http://127.0.0.1:5000/helloname/?name=mario
+    return f'<h1>Welcome on Chuck Norris Jokes app, {name}!</h1>' 
+    
+
 if __name__ == "__main__":
     app.run()
