29c42473c1134a135c753ab7c0f463ddb7466e32
tommasosansone91 <tommaso.sansone91@virgilio.it>
2024-10-27 17:45:33 +0100

    add migration gear

--- chuck_norris_jokes_caller/app.py
+++ chuck_norris_jokes_caller/app.py
@@ -1,9 +1,26 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask
+from flask import render_template
 from flask import request
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 from markupsafe import escape
- 
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'chuck_norris_jokes_caller.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app,db)
+ 
+
 
 @app.route("/")
 def index():
