ac98c28b5feed8f0b4111dd17e511825d3190609
tommasosansone91 <tommaso.sansone91@virgilio.it>
2024-10-27 20:40:31 +0100

    update database setup file

--- chuck_norris_jokes_caller/setupdatabase.py
+++ chuck_norris_jokes_caller/setupdatabase.py
@@ -6,6 +6,11 @@ from app import \
 
 from app import app
 
+from sqlalchemy.exc import SQLAlchemyError
+
+
+print(" ")
+
 with app.app_context(): 
         
     db.create_all()
@@ -14,52 +19,84 @@ with app.app_context():
 
     # add test data
 
-    joke_1 =  Joke(1)
+    try:
+        joke_1 =  Joke(1)
 
+        joke_version_1_content = """At various times during the making of Star Wars movies, Chuck Norris was considered for the parts of Han Solo, Luke Skywalker, Darth Vader, the Emperor, Chewbacca, Lando and Yoda. In each case he would be wearing his regular clothes and speak in his Texan drawl."""
 
-    joke_version_1_content = """At various times during the making of Star Wars movies, Chuck Norris was considered for the parts of Han Solo, Luke Skywalker, Darth Vader, the Emperor, Chewbacca, Lando and Yoda. In each case he would be wearing his regular clothes and speak in his Texan drawl."""
+        joke_version_1 = JokeVersion(
+            id=2,
+            joke_id=1,
+            content=joke_version_1_content
+        )
 
-    joke_version_1 = JokeVersion(
-        id=2,
-        joke_id=1,
-        content=joke_version_1_content
-    )
+        db.session.add(joke_1)
+        db.session.add(joke_version_1)
+        db.session.commit()
 
+    except SQLAlchemyError as e:
+        print(f"Managed database error: \n{e}\n")
+        db.session.rollback() 
+
+    except Exception as e:
+        print(f"An unexpected error occurred: \n{e}\n")
     
 
-    joke_2 =  Joke(2)
+    try:
+        joke_2 =  Joke(2)
 
 
-    joke_version_2_content = """as a kid Chuck Norris carried out over 11 sucsessful scuicide bombing missions for the u.s. millitary... the most well known was his attack hiroshima"""
+        joke_version_2_content = """as a kid Chuck Norris carried out over 11 sucsessful scuicide bombing missions for the u.s. millitary... the most well known was his attack hiroshima"""
 
-    joke_version_2 = JokeVersion(
-        id=4,
-        joke_id=2,
-        content=joke_version_2_content
-    )
+        joke_version_2 = JokeVersion(
+            id=4,
+            joke_id=2,
+            content=joke_version_2_content
+        )
 
+        db.session.add(joke_2)
+        db.session.add(joke_version_2)
+        db.session.commit()
 
+    except SQLAlchemyError as e:
+        print(f"Managed database error: \n{e}\n")
+        db.session.rollback() 
 
-    joke_3 =  Joke(3)
-
-
-    joke_version_3_content = """Many a class 5 Oklahoma tornado were started when Chuck Norris was preparing to lasso a steer."""
-
-    joke_version_3 = JokeVersion(
-        id=6,
-        joke_id=3,
-        content=joke_version_3_content
-    )
+    except Exception as e:
+        print(f"An unexpected error occurred: \n{e}\n")
 
 
+    try:
+        joke_3 =  Joke(3)
 
 
-    created_objects_list = [
-        joke_1, joke_version_1,
-        joke_2, joke_version_2,
-        joke_3, joke_version_3
-    ]
+        joke_version_3_content = """Many a class 5 Oklahoma tornado were started when Chuck Norris was preparing to lasso a steer."""
 
-    db.session.add_all(created_objects_list)
+        joke_version_3 = JokeVersion(
+            id=6,
+            joke_id=3,
+            content=joke_version_3_content
+        )
 
-    db.session.commit()
+        db.session.add(joke_3)
+        db.session.add(joke_version_3)
+        db.session.commit()
+
+    except SQLAlchemyError as e:
+        print(f"Managed database error: \n{e}\n")
+        db.session.rollback() 
+
+    except Exception as e:
+        print(f"An unexpected error occurred: \n{e}\n")
+
+
+
+    # created_objects_list = [
+    #     joke_1, joke_version_1,
+    #     joke_2, joke_version_2,
+    #     joke_3, joke_version_3
+    # ]
+
+    # db.session.add_all(created_objects_list)
+
+    # db.session.commit()
