7ddb293f20358069c225051bd2bd1f8c40e1201d
tommasosansone91 <tommaso.sansone91@virgilio.it>
2024-10-28 02:58:20 +0100

    added delete api

--- chuck_norris_jokes_caller/app.py
+++ chuck_norris_jokes_caller/app.py
@@ -17,7 +17,8 @@ import json
 
 from db_queries import \
     GET_JOKES_BY_FREE_TEXT, \
-    GET_JOKE_BY_JOKE_ID
+    GET_JOKE_BY_JOKE_ID, \
+    SET_JOKE_AS_NOT_ACTIVE
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -161,6 +162,51 @@ def get_jokes_by_id(joke_id):
 
     return jokes
 
+
+## `DELETE /api/jokes/{id}`
+# Endpoint to delete a joke by unique id. If the joke does not exist, return 404 not found. But if it does, mark the joke locally as deleted. Any subsequent reads should *NOT* see this joke.
+@app.route('/api/jokes/<string:joke_id>', methods=['DELETE'])
+def delete_joke_by_id(joke_id):
+
+    # caller app jokes
+    #-----------------
+
+    # http://127.0.0.1:5000/api/jokes/2
+
+    conn = sqlite3.connect(sqlite_db_path)
+    curs = conn.cursor()
+
+    # print(joke_id)
+
+    curs.execute(SET_JOKE_AS_NOT_ACTIVE, (joke_id,))
+
+    if curs.rowcount == 0:
+        # No update
+        response = {
+            "success": False,
+            "msg": f"Cannot delete Joke. Joke with id {joke_id} not found or already deleted (TIP: deactivated)."
+        }
+        status_code = 404
+    else:
+        # success
+        response = {
+            "success": True,
+            "msg": f"Joke with id {joke_id} has been set as not active."
+        }
+        status_code = 200
+
+    conn.commit()  # here I have to commit the change
+    conn.close()
+
+    return (
+        json.dumps(response), 
+        status_code, 
+        {'Content-Type': 'application/json'}
+        )
+
+
+# models
+############
 
 class Joke(db.Model):
 
