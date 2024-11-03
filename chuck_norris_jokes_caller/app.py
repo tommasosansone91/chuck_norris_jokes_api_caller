2d7063be3982256e88ba45dd997fc73c556ffa9d
tommasosansone91 <tommaso.sansone91@virgilio.it>
2024-11-03 18:19:48 +0100

    invert ommands order, add some comments

--- chuck_norris_jokes_caller/app.py
+++ chuck_norris_jokes_caller/app.py
@@ -6,19 +6,24 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 
+# set some auxiliary variable
 basedir = os.path.abspath(os.path.dirname(__file__))
-
-app = Flask(__name__)
 
 sqlite_db_path = os.path.join(basedir, 'chuck_norris_jokes_caller.sqlite')
 
+# create app
+app = Flask(__name__)
+
+# configure the DB once the app has been created
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + sqlite_db_path
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+# setup the migration system
 Migrate(app,db)
 
+# import the apis
 from api import \
     index, \
     helloname, \
@@ -28,6 +33,6 @@ from api import \
     add_joke, \
     update_joke_by_id
  
-
+# run the app
 if __name__ == "__main__":
     app.run()
